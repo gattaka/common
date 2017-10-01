f700270adf4e28d74ea469a57c0e3948aefc86c0
gattaka <gattakahynca@seznam.cz>
2017-10-01 23:41:28 +0200

    LocalDate utils

--- src/main/java/cz/gattserver/common/util/DateUtil.java
+++ src/main/java/cz/gattserver/common/util/DateUtil.java
@@ -2,6 +2,8 @@ package cz.gattserver.common.util;
 
 import org.apache.commons.lang3.time.DateUtils;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -17,4 +19,12 @@ public final class DateUtil {
 	public static Date resetTimeToMidnight(Date date) {
 		return DateUtils.addSeconds(DateUtils.addMinutes(DateUtils.addHours(resetTime(date), 23), 59), 59);
 	}
+
+	public static LocalDate toLocalDate(Date date) {
+		return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+	
+	public static java.sql.Date toDate(LocalDate locDate) {
+    	return java.sql.Date.valueOf(locDate);
+    }
 }
