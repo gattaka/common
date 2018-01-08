2903c7e95aa9b51e4f3b630b763e94a9811da3ec
gattaka <gattakahynca@seznam.cz>
2018-01-08 23:18:56 +0100

    common-test dep.; Formmatter renamed

--- src/test/java/cz/gattserver/common/util/CZAmountFormatterTest.java
+++ src/test/java/cz/gattserver/common/util/CZAmountFormatterTest.java
@@ -3,18 +3,18 @@ package cz.gattserver.common.util;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class CZSuffixFormatterTest {
+public class CZAmountFormatterTest {
 
 	@Test
 	public void testFormat() {
-		CZSuffixFormatter formatter = new CZSuffixFormatter("kostka", "kostky", "kostek");
+		CZAmountFormatter formatter = new CZAmountFormatter("%d kostka", "%d kostky", "%d kostek");
 		assertEquals("-", formatter.format(null));
 		assertEquals("-", formatter.format(0));
 		assertEquals("1 kostka", formatter.format(1));
 		assertEquals("2 kostky", formatter.format(2));
 		assertEquals("5 kostek", formatter.format(5));
 
-		formatter = new CZSuffixFormatter("kostka", "kostky", "kostek", "nic");
+		formatter = new CZAmountFormatter("%d kostka", "%d kostky", "%d kostek", "nic");
 		assertEquals("nic", formatter.format(null));
 		assertEquals("nic", formatter.format(0));
 	}
